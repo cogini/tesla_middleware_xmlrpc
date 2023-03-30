5748746f8e8ce03fc7561ed5800e4574d3881b58
Jake Morrison <jake@cogini.com>
2023-03-30 14:05:16 -0500

    Generate project

---
+++ lib/tesla_middleware_xmlrpc.ex
@@ -0,0 +1,18 @@
+defmodule TeslaMiddlewareXmlrpc do
+  @moduledoc """
+  Documentation for `TeslaMiddlewareXmlrpc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TeslaMiddlewareXmlrpc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tesla_middleware_xmlrpc_test.exs
@@ -0,0 +1,8 @@
+defmodule TeslaMiddlewareXmlrpcTest do
+  use ExUnit.Case
+  doctest TeslaMiddlewareXmlrpc
+
+  test "greets the world" do
+    assert TeslaMiddlewareXmlrpc.hello() == :world
+  end
+end
